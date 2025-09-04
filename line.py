88daf2877bd4901b88a6eccfab6ae842a3c6028e
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-09-04 14:34:25 -0300

    Update line.py

--- line.py
+++ line.py
@@ -9,13 +9,14 @@ def line():
     print(f"El coeficiente B de su ecuación de la recta es: {(coefB)}")
     print(f"El coeficiente X1 de su ecuación de la recta es: {(coefX1)}")
     print(f"El coeficiente X2 de su ecuación de la recta es: {(coefX2)}")
-
-    print("\nPara la siguiente ecuación:")
-    print(f"	Y = {coefA}X + {coefB}")
+    print("")
+    print("Para la siguiente ecuación:")
+    print(f"\tY = {coefA}X + {coefB}")
     x1 = coefA * coefX1 + coefB
     x2 = coefA * coefX2 + coefB
-    print("\nDados los siguientes puntos:")
-    print(f"	P1 ({coefX1}, {x1})")
-    print(f"	P2 ({coefX2}, {x2})")
-
-    print(f"\nLa distancia entre ellos es: {math.dist((coefX1, x1), (coefX2, x2))}")
+    print("")
+    print("Dados los siguientes puntos:")
+    print(f"\tP1 ({coefX1}, {x1})")
+    print(f"\tP2 ({coefX2}, {x2})")
+    print("")
+    print(f"La distancia entre ellos es: {math.dist((coefX1, x1), (coefX2, x2))}")
